85cdf9388c29c32cec15e2385612acaf1fd1b167
Artem Hrybanov <artemhrybanov@MacBook-Pro-Artem.local>
2022-09-12 17:32:32 +0300

    Add firebase auth and new pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+
 import { Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-      <Navbar />
+    <AuthContextProvider>
+    <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Account />} />
         </Routes>
+    </AuthContextProvider>
     </>
   );
 }
